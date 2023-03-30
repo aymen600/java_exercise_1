88ea805e67b8e2fb65065e62f923de995b790f17
aymen600 <alahouel11@gmail.com>
2023-03-30 18:59:05 +0200

    add loop

--- src/Launcher.java
+++ src/Launcher.java
@@ -3,12 +3,15 @@ public class Launcher {
     public static void main(String[] args) {
         System.out.println("Bienvenue dans le programme !");
         Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.nextLine();
+        String input = "";
 
-        if (userInput.equals("quit")) {
-            return;
+        while(!input.equals("quit")) {
+            System.out.print("> ");
+            input = scanner.nextLine();
+
+            if(!input.equals("quit")) {
+                System.out.println("Unknown command");
+            }
         }
-        System.out.println("Unknown command");
-        return;
     }
 }
