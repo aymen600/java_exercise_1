26f819d8bf1918a3168b3df359f3143ffff2863f
aymen600 <alahouel11@gmail.com>
2023-03-30 19:12:53 +0200

    add fibo

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,5 +1,13 @@
 import java.util.Scanner;
 public class Launcher {
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n-1) + fibonacci(n-2);
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Bienvenue dans le programme !");
         Scanner scanner = new Scanner(System.in);
@@ -9,9 +17,17 @@ public class Launcher {
             System.out.print("> ");
             input = scanner.nextLine();
 
-            if(!input.equals("quit")) {
+            if(!input.equals("quit") && !input.equals("fibo")) {
                 System.out.println("Unknown command");
             }
+            else if (input.equals("fibo")) {
+                System.out.print("Entrez un nombre : ");
+                int n = scanner.nextInt();
+                scanner.nextLine(); // consommer le caractère \n
+                int fibo = fibonacci(n);
+                System.out.println("Fibonacci(" + n + ") = " + fibo);
+            }
+
         }
     }
 }
